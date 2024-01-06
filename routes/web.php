02d2430f66fa3cf80a3cc5a1bf21f1242fe8e2c0
3B032063 <3B032063@gm.student.ncut.edu.tw>
2024-01-07 02:28:16 +0800

    資料模擬：呈現使用者目前所有訂單

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CartItemController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -24,6 +25,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::resource('cart_items', CartItemController::class)->middleware(['auth', 'verified']);
+Route::resource('orders', OrderController::class)->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
