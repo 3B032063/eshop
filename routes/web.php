77ea97a5ff0db74575b35aab2f2d0e83b8f50626
3B032063 <3B032063@gm.student.ncut.edu.tw>
2024-01-07 01:07:16 +0800

    產生新增的七個route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('Products', ProductController::class);
+
+/* 路由名稱所用的URL、HTTP方法(或稱HTTP動詞)、與所串接的控制器&方法：
+   product.index    GET        /                         Productontroller@index    顯示所有商品
+   product.show     GET|HEAD   Products/{Product}        Productontroller@show     檢視某一個商品
+   product.create   GET        Products/create           Productontroller@create   新增某一個商品
+   product.store    POST       Products                  Productontroller@store    儲存新增的商品
+   product.edit     GET|HEAD   Products/{Product}/edit   Productontroller@edit     修改某一個商品
+   product.update   PUT|PATCH  Products/{Product}        Productontroller@update   更新某一個商品
+   product.destroy  DELETE     Products/{Product}        Productontroller@destroy  刪除某一個商品
+*/
