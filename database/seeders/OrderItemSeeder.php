aa61f507dd7bf0a63b8543cfbd12061b0d32103c
3B032063 <3B032063@gm.student.ncut.edu.tw>
2024-01-07 02:23:41 +0800

    資料模擬：呈現使用者目前購物車的內容

--- database/seeders/OrderItemSeeder.php
+++ database/seeders/OrderItemSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +15,14 @@ class OrderItemSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Order::all()->each(function($order){
+            OrderItem::factory(3)->create([
+                'order_id' => $order->id,
+                'product_id' => function () {
+                    $randomProduct = Product::inRandomOrder()->first();
+                    return $randomProduct ? $randomProduct->id : Product::factory()->create()->id;
+                },
+            ]);
+        });
     }
 }
